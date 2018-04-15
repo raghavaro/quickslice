1ebb580834329ca4b0e2113d7cce438dadc92e03
Raghav Arora <agu94.raghav@gmail.com>
2018-04-15 16:18:30 +0200

    Added colormapping

--- slice.py
+++ slice.py
@@ -5,6 +5,7 @@ import os
 import sys
 import math
 
+
 def read_cct_file(file):
     lines = [line.rstrip('\n') for line in open(file)]
     info = map(int, lines.pop(0).split(','))
@@ -15,6 +16,7 @@ def read_cct_file(file):
         voxels.append(slice)
     volume = np.asarray(voxels)
     return volume
+
 
 def get_slice(volume, plane, threshold):
     volume[np.where(volume<threshold[0])] = -1
@@ -33,6 +35,7 @@ def get_slice(volume, plane, threshold):
         slice = volume[z,:,:]
     return slice
 
+
 def apply_transparency(slice):
     alpha_slice = np.zeros_like(slice)
     alpha_slice[np.where(slice!=-1)] = 255
@@ -43,7 +46,61 @@ def apply_transparency(slice):
     rgba_slice[..., 2] = slice
     rgba_slice[..., 3] = alpha_slice
     return rgba_slice
- 
+
+
+def apply_colormap(slice, threshold, colormap):
+    color_scales = {
+        'jet':[
+            [0,0,131],
+            [0,60,170],
+            [5,255,255],
+            [255,255,0],
+            [250,0,0],
+            [128,0,0]
+        ],
+        'viridis': [
+            [68,1,84],
+            [71,44,122],
+            [59,81,139],
+            [44,113,142],
+            [33,144,141],
+            [39,173,129],
+            [92,200,99],
+            [170,220,50],
+            [253,231,37]
+        ]
+    }
+    if colormap in color_scales:
+        lerp_range = range(threshold[0], threshold[1] + 1)
+        max_index = threshold[1]-threshold[0]
+        replacer = []
+        reds = np.zeros_like(slice)
+        blues = np.zeros_like(slice)
+        greens = np.zeros_like(slice)
+        alphas = np.zeros_like(slice)
+        for i in lerp_range:
+            replace_from = i
+            replace_to_index = (i-threshold[0])*(len(color_scales[colormap])-1)/max_index
+            upper_index = int(math.ceil(replace_to_index))
+            lower_index = int(math.floor(replace_to_index))
+            multiplier = replace_to_index - lower_index
+            upper_color = np.asarray(color_scales[colormap][upper_index])
+            lower_color = np.asarray(color_scales[colormap][lower_index])
+            this_color = np.rint(lower_color + multiplier*(upper_color-lower_color)).astype(int)
+            indices = np.where(slice == i)
+            reds[indices[0], indices[1]] = this_color[0]
+            greens[indices[0], indices[1]] = this_color[1]
+            blues[indices[0], indices[1]] = this_color[2]
+        alphas[np.where(slice!=-1)] = 255
+        rgba_slice = np.zeros((slice.shape[0], slice.shape[1], 4), 'uint8')
+        rgba_slice[..., 0] = reds
+        rgba_slice[..., 1] = greens
+        rgba_slice[..., 2] = blues
+        rgba_slice[..., 3] = alphas
+        return rgba_slice
+    sys.exit('Colormap not found')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-x', '--x', type=float)
@@ -51,6 +108,7 @@ if __name__ == '__main__':
     parser.add_argument('-z', '--z', type=float)
     parser.add_argument('-t', '--threshold', type=int, nargs='+',default=[0, 255])
     parser.add_argument('--cct', type=str, required=True)
+    parser.add_argument('-c', '--colormap', type=str)
     args = parser.parse_args()
     if (args.x and args.y) or (args.y and args.z) or (args.z and args.x):
         sys.exit('Only one of x, y, or z allowed')
@@ -67,7 +125,10 @@ if __name__ == '__main__':
     slice = get_slice(volume, plane, args.threshold)
     if slice is None:
         sys.exit('Cannot generate slice')
-    image_slice = apply_transparency(slice)
+    if args.colormap:
+        image_slice = apply_colormap(slice, args.threshold, args.colormap)
+    else:
+        image_slice = apply_transparency(slice)
     img = Image.fromarray(image_slice, 'RGBA')
     img.save('output.png')
     img.show()
