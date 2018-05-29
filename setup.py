dd5b98b229e18f7b44cbfccbeffc8ae2c3249fa4
Raghav Arora <agu94.raghav@gmail.com>
2018-05-29 16:07:24 +0200

    Made changes to setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
-    name='quickslice',
+    name="quickslice",
     version=1.2,
-    author='Raghav Arora',
-    author_email='agu94.raghav@gmail.com',
-    url='http://github.com/raghavaro/quickslice',
+    author="Raghav Arora",
+    author_email="agu94.raghav@gmail.com",
+    url="http://github.com/raghavaro/quickslice",
     description="Extract slices from compressed volumetric data",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license='MIT',
+    license="MIT",
     packages=find_packages(),
-    scripts=['qslice'],
+    scripts=["qslice"],
     install_requires=[
-        'Pillow==5.1.0',
-        'numpy==1.14.3'
+        "Pillow==5.1.0",
+        "numpy==1.14.3"
     ],
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 2.7",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
+        "Operating System :: OS Independent"
+    ],
 )
